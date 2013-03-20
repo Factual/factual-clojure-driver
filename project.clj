802cc5fb6338e306410e95c236db77dc6a255f45
Donald Clark Jackson <dcj@clark-communications.com>
2013-03-20 00:08:55 -0700

    Updated a few dependencies, and replaces usage of data.json with Cheshire

--- project.clj
+++ project.clj
@@ -2,14 +2,16 @@
   :url "http://github.com/Factual/factual-clojure-driver"
   :description "Officially supported Clojure driver for Factual's public API"
   :dependencies [
-    [org.clojure/clojure "1.4.0"]
-    [org.clojure/data.json "0.1.2"]
-    [slingshot "0.10.2"]
+    [org.clojure/clojure "1.5.1"]
+    [factual/sosueme "0.0.14"]                    
+    [slingshot "0.10.3"]
     [oauth-clj "0.1.2"
      :exclusions [org.slf4j/slf4j-log4j12]]
     [clj-http "0.5.2"]] ;oauth-clj has dependency on clj-http as well, but the debug feature for
                         ;that version didn't work
-  :dev-dependencies [[factual/sosueme "0.0.14"]
-                     [lein-clojars "0.6.0"]
-                     [oauth-clj "0.1.2"
-                      :exclusions [org.slf4j/slf4j-log4j12]]])
+  )
+
+;; :dev-dependencies [[factual/sosueme "0.0.14"]
+;;                      [lein-clojars "0.6.0"]
+;;                      [oauth-clj "0.1.2"
+;;                       :exclusions [org.slf4j/slf4j-log4j12]]])
